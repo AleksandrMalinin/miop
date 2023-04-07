ae00826c1ee7ed869085c69bc41de360ad0a65aa
AleksandrMalinin <alexander.s.malinin@gmail.com>
2023-04-07 02:48:24 +0100

    Fix next.config.js etc.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/out",
+  basePath: "",
+  assetPrefix: "",
   images: {
     unoptimized: true
   }
