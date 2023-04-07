6a3e4f868d5a9e1f4a4ec7d31b770553a38b7014
AleksandrMalinin <alexander.s.malinin@gmail.com>
2023-04-07 01:11:52 +0100

    Optimyze imges, update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "out"
+  basePath: "/out",
+  images: {
+    optimizeImages: false
+  }
 }
 
 module.exports = nextConfig
